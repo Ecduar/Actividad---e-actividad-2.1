0c8d2fcdf8d27f803b72571606c17157287bfcdd
Autorgus777 <mendezchacinga@uvm.edu.ve>
2024-06-22 23:23:26 -0400

    Modelo base de datos | userCooperative

---
+++ src/models/userCooperative.js
@@ -0,0 +1,16 @@
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const User = require("./user");
+const Cooperative = require("./cooperative");
+
+const UserCooperative = sequelize.define("UserCooperative", {
+  role: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+User.belongsToMany(Cooperative, { through: UserCooperative });
+Cooperative.belongsToMany(User, { through: UserCooperative });
+
+module.exports = UserCooperative;
