f59209a532d98ff5a3eb3fce327d63730e6c4f46
Autorgus777 <mendezchacinga@uvm.edu.ve>
2024-06-23 13:58:29 -0400

    Correciones de codigo

--- src/models/cooperative.js
+++ src/models/cooperative.js
@@ -1,41 +1,11 @@
-const { Sequelize } = require("sequelize");
-const dotenv = require("dotenv");
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
 
-dotenv.config();
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  process.env.DB_PASS,
-  {
-    host: process.env.DB_HOST,
-    dialect: "mysql"
+const Cooperative = sequelize.define("Cooperative", {
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
   }
-);
+});
 
-// Cargar modelos
-const User = require("./models/User")(sequelize, Sequelize);
-const UserCooperative = require("./models/UserCooperative")(
-  sequelize,
-  Sequelize
-);
-const SavingAccount = require("./models/SavingAccount")(sequelize, Sequelize);
-const LoanAccount = require("./models/LoanAccount")(sequelize, Sequelize);
-const Cooperative = require("./models/Cooperative")(sequelize, Sequelize);
-
-// Exportar sequelize para usarlo en otros archivos
-module.exports = sequelize;
-
-// Función para sincronizar la base de datos con las migraciones
-async function syncDatabase() {
-  try {
-    // Ejecutar migraciones (crear tablas si no existen)
-    await sequelize.sync();
-    console.log("Base de datos sincronizada con éxito.");
-  } catch (error) {
-    console.error("Error al sincronizar la base de datos:", error);
-  }
-}
-
-// Ejecutar la sincronización al iniciar la aplicación
-syncDatabase();
+module.exports = Cooperative;
